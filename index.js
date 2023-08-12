fc797d176f216b583f80e63a9994266ff1d5b224
seljaammu <selja.ksf@gmail.com>
2023-08-12 14:50:16 +0530

    variables

---
+++ index.js
@@ -0,0 +1,69 @@
+
+//   console.log('Hello World');
+
+//   let name ='selja'
+//   let age =24
+//   let color = null
+
+let person = {
+    name :'selja',
+    age : 30
+}
+person.name =' mary';
+person['age']= 20;
+
+// console.log(person)
+// console.log(peroson.name)
+// console.log(person.name)
+
+// let selectedColors = ['red','green']
+// selectedColors[2] = 'pink'
+// console.log(selectedColors.length)
+
+
+// function greet() {
+//     console.log('Hello World')
+
+// }
+
+// greet();
+
+
+// function greet(name)
+// {
+//     console.log('hello ' + name)
+// }
+
+// greet('kani');
+
+// function square(number)
+// {
+//     return number*number;
+
+// }
+//  let number = square(2)
+
+//  console.log(number);
+
+
+let age =10;
+console.log(age);
+ a =10;
+ console.log(a);
+ document.write(a);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
