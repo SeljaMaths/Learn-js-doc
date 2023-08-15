defffc7202384b6dead96bb519c9a8b2ddb18731
seljaammu <selja.ksf@gmail.com>
2023-08-15 18:03:28 +0530

    var,let and const

---
+++ var.js
@@ -0,0 +1,88 @@
+
+// var myname ='selja';
+// function whatisyourname(){
+//     console.log(myname);
+// }
+// console.log(myname);
+// whatisyourname()
+
+// local scope
+// function whatisyourname(){
+//     var myname ='selja';
+//     console.log(myname);
+// }
+// console.log(myname);
+
+// whatisyourname()
+
+//upadte variabl
+// var lang;
+// lang ='english';
+// console.log(lang);
+// lang ='kannda';
+// console.log(lang)
+
+
+//let
+// let colour1 ='red';
+// function colordata(){
+//     let colur2 ='green';
+//     console.log(colour1);
+//     console.log(colur2);
+// }
+// colordata();
+
+// let color1 ='red';
+// function colordata(){
+//     let color2 ='orange';
+//     console.log(color1);
+//     if (1)
+//     {
+//         console.log(color2);
+//     }
+// }
+// colordata();
+
+// var myname= 'selja';
+// var myname ='kaviya';
+// console.log(myname);
+
+// let myname= 'selja';
+// let myname ='kaviya';
+// console.log(myname);
+
+// let color1 ='red';
+// function color_data(){
+//     console.log(color1);
+//     if(1){
+//         let color1 ='green';
+//         console.log(color1);
+
+//     }
+// }
+// color_data();
+
+// const lang =1;
+// console.log(lang)
+
+
+// const color="red";
+// function colordata(){
+//     console.log(color);
+//     if(1){
+//         const color="orange";
+//         console.log(color);
+
+//     }
+// }
+// colordata();
+
+var a =80;
+{
+    var a ='guru';
+}
+let b =a;
+{
+    let b ="seema";
+}
+console.log(b);
